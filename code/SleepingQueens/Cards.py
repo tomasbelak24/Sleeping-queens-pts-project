fb704d5d7bbbfea40cd2be22080776c3f61283d3
Tomas Belak <tomas24belak@gmail.com>
2022-11-24 22:44:25 +0100

    modifed CardType to be represented as str rather than enum

--- code/SleepingQueens/Cards.py
+++ code/SleepingQueens/Cards.py
@@ -2,13 +2,14 @@ from dataclasses import dataclass
 from enum import Enum
 
 
-class CardType(Enum):
-    NUMBER = 1
-    KING = 2
-    KNIGHT = 3
-    SLEEPING_POTION = 4
-    DRAGON = 5
-    MAGIC_WAND = 6
+@dataclass
+class CardType():
+    _cardTypes = ["NUMBER",
+                  "KING",
+                  "KNIGHT",
+                  "SLEEPING_POTION",
+                  "DRAGON",
+                  "MAGIC_WAND"]
 
 
 @dataclass
