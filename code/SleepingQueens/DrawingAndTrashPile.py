de8d25af837f791ce5da9a6f9a09c7828be07e3e
Tomas Belak <tomas24belak@gmail.com>
2022-11-25 22:08:10 +0100

    commit probably not 1 but say commit1:     modified generateCards in DrawingAndTrashPile         + Cards are now generated with unique value

--- code/SleepingQueens/DrawingAndTrashPile.py
+++ code/SleepingQueens/DrawingAndTrashPile.py
@@ -14,19 +14,19 @@ class DrawingAndTrashPile:
     def generateCards(self, arr: List[Card]) -> None:
         for i in range(8):
             arr.append(
-                Card(CardType.KING, CardType.KING.value))
+                Card(CardType.KING, i+1))
         for i in range(4):
             arr.append(
-                Card(CardType.KNIGHT, CardType.KNIGHT.value))
+                Card(CardType.KNIGHT, i+1))
         for i in range(4):
             arr.append(
-                Card(CardType.SLEEPING_POTION, CardType.SLEEPING_POTION.value))
+                Card(CardType.SLEEPING_POTION, i+1))
         for i in range(3):
             arr.append(
-                Card(CardType.MAGIC_WAND, CardType.MAGIC_WAND.value))
+                Card(CardType.MAGIC_WAND, i+1))
         for i in range(3):
             arr.append(
-                Card(CardType.DRAGON, CardType.DRAGON.value))
+                Card(CardType.DRAGON, i+1))
         for i in range(4):
             for j in range(1, 11,1):
                 arr.append(Card(CardType.NUMBER, j))
