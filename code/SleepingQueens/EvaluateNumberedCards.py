ec9e7d01ff0c3218fd66cd87b140c40541250959
Tomas Belak <tomas24belak@gmail.com>
2022-11-30 20:31:40 +0100

    commit9 EvaluateNumberedCards implemented     + init     + play

---
+++ code/SleepingQueens/EvaluateNumberedCards.py
@@ -0,0 +1,20 @@
+from Cards import Card
+from typing import List
+
+
+class EvaluateNumberedCards:
+
+    def __init__(self, cards: List[Card]) -> None:
+        self.cards: List[Card] = cards
+
+    def play(self) -> bool:
+        numOfCards: int = len(self.cards)
+        if numOfCards == 2:
+            return self.cards[0].value == self.cards[1].value
+        else:
+            eq = sorted(self.cards, key = lambda x: x.value)
+            sum = 0
+            for card in eq[:-1]:
+                sum += card.value
+
+            return sum == eq[-1].value
