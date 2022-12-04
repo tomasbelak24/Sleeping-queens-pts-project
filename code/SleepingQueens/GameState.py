7e7ae8dd2d10967c20b1703aafcb8aa36b43b30f
Tomas Belak <tomas24belak@gmail.com>
2022-12-04 16:05:07 +0100

    commit22 first iteration of GameState class

---
+++ code/SleepingQueens/GameState.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import List, Set, Dict, Optional
+from SleepingQueens import SleepingQueens
+from DataType_Positions import SleepingQueenPosition, HandPosition, AwokenQueenPostion
+from Cards import Card, Queen
+
+@dataclass
+class GameState:
+    numberOfPlayers: int
+    onTurn: int
+    sleepingQueens: Set[SleepingQueenPosition]
+    awokenQueens: Dict[AwokenQueenPostion, Queen]
+    cards: Dict[HandPosition, Optional[Card]]
+    cardsDiscardedLastTurn: List[Card]
