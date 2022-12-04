e6be03add345db7c84fe754dede61b6215b3edc6
Tomas Belak <tomas24belak@gmail.com>
2022-12-04 15:14:19 +0100

    commit15 adapted EvaluateNumberedCards to new design

--- code/SleepingQueens/EvaluateNumberedCards.py
+++ code/SleepingQueens/EvaluateNumberedCards.py
@@ -4,15 +4,12 @@ from typing import List
 
 class EvaluateNumberedCards:
 
-    def __init__(self, cards: List[Card]) -> None:
-        self.cards: List[Card] = cards
-
-    def play(self) -> bool:
-        numOfCards: int = len(self.cards)
+    def play(self, cards: List[Card]) -> bool:
+        numOfCards: int = len(cards)
         if numOfCards == 2:
-            return self.cards[0].value == self.cards[1].value
+            return cards[0].value == cards[1].value
         else:
-            eq: List[Card] = sorted(self.cards, key = lambda x: x.value)
+            eq: List[Card] = sorted(cards, key = lambda x: x.value)
             sum: int = 0
             for card in eq[:-1]:
                 sum += card.value
