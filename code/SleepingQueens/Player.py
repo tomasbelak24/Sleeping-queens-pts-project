d7cb781f41f214147d65c827f7fa88ab9cb13aa6
Tomas Belak <tomas24belak@gmail.com>
2022-12-04 16:06:28 +0100

    commit24 first iteration of Player class

--- code/SleepingQueens/Player.py
+++ code/SleepingQueens/Player.py
@@ -1,44 +1,46 @@
 from DataType_Positions import HandPosition
-import DrawingAndTrashPile, SleepingQueens, Hand
+from Hand import Hand
 from Game import Game
-from typing import List
-import EvaluateAttack
-import EvaluateNumberedCards
-from Cards import CardType, Card
+from typing import List, Position, Dict
+from EvaluateAttack import EvaluateAttack
+from EvaluateNumberedCards import EvaluateNumberedCards
+from Cards import CardType, Card, Queen
+from GameState import GameState
+from Queens import AwokenQueens
 
 class Player:
 
     def __init__(self) -> None:
-        self._hand: Hand = Hand(Game.getNumOfPlayers())
-        Game.addPlayer(self)
+        self.playerHand: Hand = Hand(GameState.numberOfPlayers)
+        self.awokenQueens: Dict[int, Queen] = AwokenQueens()
 
     
-    def play(self, cardPositions: List[int]) -> None:
+    def play(self, cardPositions: List[Position], targetQueen: Position = -1, targetPlayerIdx: int = -1) -> None:
         numberOfCards: int = len(cardPositions)
-        cards: List[Card] = [self._hand[pos] for pos in cardPositions]
+        cards: List[Card] = [self.playerHand._hand[pos] for pos in cardPositions]
 
         if numberOfCards == 1 and cards[0].isAttacking():
             # playing attacking card
             # evaluate attack
-            typeOfCard = cards[0].type
-            eA = EvaluateAttack(type)
-            if typeOfCard == CardType.KING:
-                eA.play()   # tuto musi ist do argumentu index kralovny ktoru chce hrac zobrat ale do pici
-                ...         # odkial mam vediet aky ten index je.. napalim to tam v testoch
-            elif typeOfCard == CardType.KNIGHT:
-                eA.play()
-                ...
-            elif typeOfCard == CardType.SLEEPING_POTION:
-                eA.play()
-                ...
+            typeOfCard: CardType = cards[0].type
+            eA: EvaluateAttack = EvaluateAttack(typeOfCard)
+            check: bool = eA.play(self.playerHand.playerIdx,targetQueen, targetPlayerIdx)
+            if check:
+                self.playerHand.pickCards(cardPositions)
+                self.playerHand.removePickedCardsAndRedraw(self.playerHand.returnPickedCards())
+                
+        elif numberOfCards == 1 and not cards[0].isAttacking():
+            raise "Player is not allowed to play a non-attacking card during his turn"
+            
         elif all(list(map(lambda x: x.type == CardType.NUMBER, cards))):
-            #musia byt numbered karty
-            #evaluate numbered cards
-            eN = EvaluateNumberedCards()
-            eN.play()
-            ...
+            eN = EvaluateNumberedCards(cards)
+            check: bool = eN.play()
+            if check:
+                self.playerHand.pickCards(cardPositions)
+                self.playerHand.removePickedCardsAndRedraw(self.playerHand.returnPickedCards())
+        
 
     def getPlayerState(self) -> None:
-        ...
+        return (self.playerHand._hand, self.awokenQueens)
 
     
