f8007e35896d4170d242be20c870c184ea381162
Tomas Belak <tomas24belak@gmail.com>
2022-12-04 15:12:49 +0100

    commit14 implemented first version of EvaluateAttack

--- code/SleepingQueens/EvaluateAttack.py
+++ code/SleepingQueens/EvaluateAttack.py
@@ -1,10 +1,15 @@
 from Cards import Card, CardType
 from Hand import Hand
 from Player import Player
+from Game import Game
+from DataType_Positions import HandPosition
+from typing import Position
 
 class EvaluateAttack:
     
     def __init__(self, cardType: CardType) -> None:
+        self.attackingCardType: CardType = cardType
+        
         if cardType == CardType.KING:
            self.defenseCardType = None
         
@@ -15,8 +20,43 @@ class EvaluateAttack:
             self.defenseCardType = CardType.MAGIC_WAND
         
     
-    def play(self, Queen: int = -1, targetPlayerIdx: int = -1) -> bool:
-        ...
+    def play(self,attackingPlayerIdx: int, targetQueen: Position = -1, targetPlayerIdx: int = -1) -> bool:
+        if self.attackingCardType == CardType.KING:
+            if Game.sleepingQueens.getQueens().keys() and targetQueen in range(12):
+                Game.players[attackingPlayerIdx].awokenQueens.addQueen(Game.sleepingQueens._sleepingQueens[targetQueen])
+                Game.sleepingQueens._sleepingQueens[targetQueen] = None
+        
+        if self.attackingCardType == CardType.KNIGHT:
+            defenseCardIndex: HandPosition = Game.players[targetPlayerIdx].playerHand.hasCardOfType(self.defenseCardType)
+            if defenseCardIndex is None:
+                Game.players[attackingPlayerIdx].awokenQueens.addQueen(
+                    Game.players[targetPlayerIdx].awokenQueens[targetQueen]
+                )
+                Game.players[targetPlayerIdx].awokenQueens.removeQueen(targetQueen)
+            else:
+                Game.players[targetPlayerIdx].playerHand.pickCards([defenseCardIndex])
+                Game.players[targetPlayerIdx].playerHand.removePickedCardsAndRedraw(
+                    Game.players[targetPlayerIdx].playerHand.returnPickedCards()
+                )
+        
+        if self.attackingCardType == CardType.SLEEPING_POTION:
+            defenseCardIndex: HandPosition = Game.players[targetPlayerIdx].playerHand.hasCardOfType(
+                self.defenseCardType)
+            if defenseCardIndex is None:
+                Game.sleepingQueens.addQueen(
+                    Game.players[targetPlayerIdx].awokenQueens[targetQueen]
+                )
+                Game.players[targetPlayerIdx].awokenQueens.removeQueen(
+                    targetQueen)
+            else:
+                Game.players[targetPlayerIdx].playerHand.pickCards(
+                    [defenseCardIndex])
+                Game.players[targetPlayerIdx].playerHand.removePickedCardsAndRedraw(
+                    Game.players[targetPlayerIdx].playerHand.returnPickedCards()
+                )
+        
+        return True 
+                
 
     def playedKing(self):
         return self.defenseCardType is None
