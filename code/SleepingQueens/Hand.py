ba12282fcb3e6cde4bae4e4bd9f0eb7ec3a1e977
Tomas Belak <tomas24belak@gmail.com>
2022-12-04 12:18:39 +0100

    commit13 reworked Hand     + incorporated HandPosition datatype     + improved cooperation with DrawingAndTrashPile

--- code/SleepingQueens/Hand.py
+++ code/SleepingQueens/Hand.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 from Cards import Card, CardType
 from DrawingAndTrashPile import DrawingAndTrashPile
 from DataType_Positions import HandPosition
@@ -7,39 +7,45 @@ class Hand:
     
     def __init__(self, playerIdx) -> None:
         self.playerIdx: int = playerIdx
-        self._hand: int
+        self._hand: Dict[HandPosition, Card]
     
     
-    def pickCards(self, positions: List[int]) -> Optional[List[Card]]:
+    def pickCards(self, positions: List[HandPosition]) -> Optional[List[Card]]:
         howManyPicked: int = len(positions)
         if howManyPicked == 0:
             return
 
         for pos in positions:
-            if pos not in range(5):
+            if pos.getCardIndex() not in range(5):
                 return
 
-        self._pickedCards = [card for i, card in enumerate(
-            self._hand) if i in positions]
+        self._pickedCards: List[Card] = []
+        for pos in self._hand.keys():
+            if pos in positions:
+                self._pickedCards.append(self._hand(pos))
+
         return self._pickedCards
 
 
-    def removePickedCardsAndRedraw(self) -> List[Card]:
+    def removePickedCardsAndRedraw(self) -> Dict(HandPosition, Card):
         pickedCards = self.returnPickedCards()
         newCards = DrawingAndTrashPile.discardAndDraw(pickedCards)
-        return newCards
+        discardedCards = list(DrawingAndTrashPile.getCardsDiscardedThisTurn())
+        for pos in self._hand.keys:
+            if self._hand[pos] in discardedCards:
+                self._hand[pos] = newCards.pop(0)
+        return self._hand
 
 
     def returnPickedCards(self) -> List[Card]:
         return self._pickedCards
 
     
-    def hasCardOfType(self, type: CardType) -> int:
-        for i, card in enumerate(self._hand):
-            if card.type == type:
-                return i
+    def hasCardOfType(self, type: CardType) -> HandPosition:
+        for pos in self._hand.keys():
+            if self._hand[pos].type == type:
+                return pos
         return
-    
 
-    def getGards(self) -> List[Card]:
-        return self._hand
+    def getCards(self) -> List[Card]:
+        return list(self._hand.values())
