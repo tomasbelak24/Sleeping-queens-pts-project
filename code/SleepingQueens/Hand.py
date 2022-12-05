a8671df0c362c6990a5848f0beb467a5397eb63c
Tomas Belak <tomas24belak@gmail.com>
2022-12-05 02:10:48 +0100

    commit32 Hand modified     changed Idx type from HandPosition to int     because HandPosition is not hashable without     furter coding

--- code/SleepingQueens/Hand.py
+++ code/SleepingQueens/Hand.py
@@ -1,37 +1,37 @@
 from typing import List, Optional, Dict
 from Cards import Card, CardType
 from DrawingAndTrashPile import DrawingAndTrashPile
-from DataType_Positions import HandPosition
 
 class Hand:
     
-    def __init__(self, playerIdx) -> None:
+    def __init__(self, playerIdx, drawing: DrawingAndTrashPile) -> None:
         self.playerIdx: int = playerIdx
-        self._hand: Dict[HandPosition, Card]
+        self._hand: Dict[int, Card] = dict()
+        self.drawing: DrawingAndTrashPile = drawing
     
     
-    def pickCards(self, positions: List[HandPosition]) -> Optional[List[Card]]:
+    def pickCards(self, positions: List[int]) -> Optional[List[Card]]:
         howManyPicked: int = len(positions)
         if howManyPicked == 0:
             return
 
         for pos in positions:
-            if pos.getCardIndex() not in range(5):
+            if pos not in range(5):
                 return
 
         self._pickedCards: List[Card] = []
         for pos in self._hand.keys():
             if pos in positions:
-                self._pickedCards.append(self._hand(pos))
+                self._pickedCards.append(self._hand[pos])
 
         return self._pickedCards
 
 
-    def removePickedCardsAndRedraw(self) -> Dict(HandPosition, Card):
+    def removePickedCardsAndRedraw(self) -> Dict[int, Card]:
         pickedCards = self.returnPickedCards()
-        newCards = DrawingAndTrashPile.discardAndDraw(pickedCards)
-        discardedCards = list(DrawingAndTrashPile.getCardsDiscardedThisTurn())
-        for pos in self._hand.keys:
+        newCards = self.drawing.discardAndDraw(pickedCards)
+        discardedCards = list(self.drawing.getCardsDiscardedThisTurn())
+        for pos in self._hand.keys():
             if self._hand[pos] in discardedCards:
                 self._hand[pos] = newCards.pop(0)
         return self._hand
@@ -41,7 +41,7 @@ class Hand:
         return self._pickedCards
 
     
-    def hasCardOfType(self, type: CardType) -> Optional[HandPosition]:
+    def hasCardOfType(self, type: CardType) -> Optional[int]:
         for pos in self._hand.keys():
             if self._hand[pos].type == type:
                 return pos
@@ -49,3 +49,14 @@ class Hand:
 
     def getCards(self) -> List[Card]:
         return list(self._hand.values())
+
+""" dp = DrawingAndTrashPile()
+hand = Hand(1, dp)
+cards = []
+for i in range(1, 6):
+    card = Card(CardType.NUMBER, i)
+    hand._hand[i] = card
+    cards.append(card)
+
+pickedCards = hand.pickCards([1, 2, 3])
+hand.removePickedCardsAndRedraw() """
