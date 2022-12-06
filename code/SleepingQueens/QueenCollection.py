8b2b4e356419a26a7f9f1a676fcc73a6a54d872b
Tomas Belak <tomas24belak@gmail.com>
2022-12-06 23:15:11 +0100

    commit38 changes made to QueenCollection

--- code/SleepingQueens/QueenCollection.py
+++ code/SleepingQueens/QueenCollection.py
@@ -1,14 +1,16 @@
 from Cards import Queen
 from DataType_Positions import SleepingQueenPosition, AwokenQueenPostion
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List
 
 class QueenCollection:
 
     def __init__(self) -> None:
         self.collection: Dict[Any, Queen] = dict()
 
-    def addQueen(self, key: Any, queen: Queen):
-        self.collection[key] = queen
+    def addQueen(self, queen: Queen):
+        maxKey = max(self.collection.keys()) + 1
+        self.collection[maxKey] = queen
+        self.indexRefresh()
 
     def removeQueen(self, position: SleepingQueenPosition) -> Optional[Queen]:
         try:
@@ -17,8 +19,18 @@ class QueenCollection:
             return
         
         del self.collection[position]
-
+        self.indexRefresh()
         return queen
 
     def getQueens(self) -> Dict[Any, Queen]:
         return self.collection
+    
+    def indexRefresh(self) -> None:
+        tempColl: Dict[Any, Queen] = dict()
+        for i, key in enumerate(self.collection.keys()):
+            tempColl[i] = self.collection[key]
+        self.collection = tempColl.copy()
+
+
+
+
